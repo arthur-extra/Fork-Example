8308cba694fac63d2a7df97455af7720a1ff5d0a
zarthur-messages <arthur.neuman@yahoo.com>
2016-09-19 22:10:21 -0400

    Remove line in preparation for demonstration

--- src/com/arthurneuman/Main.java
+++ src/com/arthurneuman/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is an example that will be used to " +
                 "demonstrate forking and pull requests.");
-        System.out.println("A great line!");
     }
 }
