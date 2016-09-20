1a695f5978ffda302b8687c5fb1703f4da64bec3
zarthur-messages <arthur.neuman@yahoo.com>
2016-09-20 18:36:29 -0400

    fix punctuation

--- src/com/arthurneuman/Main.java
+++ src/com/arthurneuman/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is an example that will be used to " +
                 "demonstrate forking and pull requests.");
-        System.out.println("This is the end of the example.");
+        System.out.println("This is the end of the example!");
     }
 }
